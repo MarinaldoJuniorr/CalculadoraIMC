5e5948212afdad26e2bf837ed539eee787c8b6b3
MarinaldoJuniorr <marinaldosfs17@gmail.com>
2024-12-24 19:33:51 +0000

    Mostrando dados na nova tela

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -3,11 +3,8 @@ package com.example.calculadoraimc
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
