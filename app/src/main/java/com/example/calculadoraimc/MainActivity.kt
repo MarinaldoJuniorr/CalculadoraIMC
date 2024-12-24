fc1bf8b284b2c8d6bf08a357582f7e83af9a625c
MarinaldoJuniorr <marinaldosfs17@gmail.com>
2024-12-24 15:26:11 +0000

    Tela inicial

---
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.calculadoraimc
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets}
+
+        val edtPeso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
+
+        val btncalcular = findViewById<Button>(R.id.button)
+
+        btncalcular.setOnClickListener {
+            val peso = edtPeso.text
+            val altura = edtAltura.text
+
+
+            println("Marinaldo acao do botao")
+        }
+    }
+}
